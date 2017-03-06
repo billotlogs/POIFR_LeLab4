02921c4b4f987df35fa67e20b59a24578410ffd4
Francis <billotlogs@gmail.com>
2017-03-06 15:32:14 -0500

    Bug a corriger -> ListCours est vide au debut et fait crasher le programme. Début de chance qu'un événement se produise.

--- LeLab4/app/src/main/java/info/dicj/lelab4/Evenement.java
+++ LeLab4/app/src/main/java/info/dicj/lelab4/Evenement.java
@@ -1,14 +1,40 @@
 package info.dicj.lelab4;
 
+import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
+
+import java.util.Random;
+
 /**
  * Created by utilisateur on 30/01/2017.
  */
 public class Evenement {
+    Random rnd;
 
-    public Evenement(){
+    public Evenement() {
+        rnd = new Random();
+    }
+
+    public void GenererEvent(){
 
     }
 
+    private void EvenementCours(){
+        int chance = rnd.nextInt(100);
+
+        switch(chance){
+
+        }
+    }
+
+    public void ChanceRealisation(int chanceSucces){
+        int chance = rnd.nextInt(101 - chanceSucces) + chanceSucces;
+
+        if(chance == 100){
+            Log.i("Test", "Fuck you");
+        }
+        Log.i("Test", "Chance : " + chance);
+    }
 
     private void NouveauDevoir(Cours cours){
         switch(cours.getNom()){
